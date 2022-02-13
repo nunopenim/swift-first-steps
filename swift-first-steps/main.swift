b720ace217d1e7c9003602877c8368df9e64a697
nunopenim <nuno.penim@gmail.com>
2022-02-13 21:26:01 +0000

    Initial commit: first steps

--- swift-first-steps/main.swift
+++ swift-first-steps/main.swift
@@ -1,11 +1,28 @@
-//
-//  main.swift
-//  swift-first-steps
-//
-//  Created by Nuno Penim on 13/02/2022.
-//
-
 import Foundation
 
-print("Hello, World!")
+print("Nunos swift test, write something ")
+var lineFromConsole = readLine()
+print("You wrote: " + lineFromConsole!)
 
+print("write 'swift' in the terminal")
+var text = ""
+
+//while é normal, se esquecermos as condições não terem parentesis
+//o equivalente ao do-while é "repeat-while"
+
+while text != "swift" {
+    text = readLine()!
+    if text != "swift" {
+        print("I told you to write 'swift'! Try again")
+    }
+}
+print("Okay, bye")
+
+// pequeno teste de loops for
+// parece semelhante a python/kotlin
+
+let thingsArray = ["thing1","thing2","thing3","thing4"]
+
+for i in thingsArray {
+    print(i)
+}
